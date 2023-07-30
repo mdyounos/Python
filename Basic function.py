5a5126b2446b613cba0ab9372d2997b703837c3b
mdyounos <194028@student.duet.ac.bd>
2023-07-30 08:39:59 +0600

    Build a basic function in python

---
+++ Basic function.py
@@ -0,0 +1,21 @@
+#A function for add two number
+def addTwo(x,y):
+    sum= x+y
+    print(sum)
+
+
+
+#A function for add three number
+def addThree(x,y,z):
+    sum=x+y+z
+    print(sum)
+    
+    
+    
+#No parameter function
+def fun():
+    print("This is a no parameteried function.")
+    
+addTwo(10,20)
+addThree(10,20,60)
+fun()
